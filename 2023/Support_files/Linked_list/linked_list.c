4cb298fe6c9286e6e8f695b74e9b4a1c818e406d
Marco-Schneider <marco.schneider452@gmail.com>
2023-12-24 22:31:42 -0300

    Minor adjustments

--- 2023/Support_files/Linked_list/linked_list.c
+++ 2023/Support_files/Linked_list/linked_list.c
@@ -11,33 +11,33 @@ List* create() {
     Inserts a given k key into a linked list l
 */
 List* insert_back(List *l, char k) {
-  if (l == NULL) {
-    l = (Node*)malloc(sizeof(Node));
-    l->data = k;
-    l->next = NULL;
-  }
-  else {
-    l->next = insert_back (l->next, k);
-  }
-  return l;
+    if (l == NULL) {
+        l = (Node*)malloc(sizeof(Node));
+        l->data = k;
+        l->next = NULL;
+    }
+    else {
+        l->next = insert_back (l->next, k);
+    }
+    return l;
 }
 
 /* 
     Removes a given k key from a linked list
 */
 List* removek(List *l, char k) {
-  if(l == NULL) {
-    return NULL;
-  }
-  else if(l->data == k) {
-    List *temp = l;
-    l = l->next;
-    free(temp);
-  }
-  else {
-    l->next = removek(l->next, k);
-  }
-  return l;
+    if(l == NULL) {
+        return NULL;
+    }
+    else if(l->data == k) {
+        List *temp = l;
+        l = l->next;
+        free(temp);
+    }
+    else {
+        l->next = removek(l->next, k);
+    }
+    return l;
 }
 
 /* 
@@ -45,10 +45,10 @@ List* removek(List *l, char k) {
     and prints its content to the user
 */
 void print_list(List* l) {
-  if (l != NULL) {
-    printf("%d ", l->data);
-    print_list(l->next);      
-  }
+    if (l != NULL) {
+        printf("%d ", l->data);
+        print_list(l->next);      
+    }
 }
 
 /* 
@@ -56,8 +56,8 @@ void print_list(List* l) {
     returning the memory that was allocated
 */
 void destroy(List *l) {
-  if(l != NULL) {
-    destroy(l->next);
-    free(l);
-  }	
+    if(l != NULL) {
+        destroy(l->next);
+        free(l);
+    }	
 }
